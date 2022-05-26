acb2057d50ce683cae00157715d6c54da74f943e
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2022-05-26 14:23:08 -0300

    style(index.ts): added libs to use with express for default

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import express from 'express'
 import compression from 'compression'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
+app.use(cors())
 app.use(compression())
 
 const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
+app.get('/', (_, res) => {
     res.status(200).json({
         status: 'success',
-        message: "Project is working : )"
+        message: 'Project is working : )'
     })
 })
 
