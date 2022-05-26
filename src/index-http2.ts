6e370c992707b1d901f73cc0ce0f50a86e92c028
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2022-05-26 15:33:03 -0300

    feat(express): added compatibility with HTTP/2.0(SPDY)

---
+++ src/index-http2.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import compression from 'compression'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import fs from 'fs'
+import spdy from 'spdy'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(compression())
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (_, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Project is working : )'
+    })
+})
+
+const server = spdy.createServer({
+    cert: fs.readFileSync(__dirname + '/../server.crt'),
+    key: fs.readFileSync(__dirname + '/../server.key')
+}, app)
+
+server.listen(PORT)
