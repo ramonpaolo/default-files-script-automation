04777ee56235bbca5ad81d7c99164bb0568b03b4
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2022-12-21 20:31:38 -0300

    feat: add type for env's

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,11 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             PORT: number;
+
+            NODE_ENV: "test" | "development" | "production";
+
+            // DATADOG
+            DATADOG_API_KEY: string = "";
         }
     }
 }
