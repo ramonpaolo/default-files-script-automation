14b08b4bc54840b10a14c3d51357b84c36c9945a
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2022-12-20 23:13:45 -0300

    feat: add redis with docker

---
+++ src/settings/redis.docker.settings.ts
@@ -0,0 +1,32 @@
+import Redis from 'ioredis'
+
+// Utils
+import loggerError from '../utils/logger_error.utils'
+
+const redis = new Redis('redis://redis')
+
+const set = async (key: string, value: string, ttl: number = 3000) => {
+    try {
+        const wasSetedWithSuccess = await redis.setex(key, ttl, value)
+
+        return !!wasSetedWithSuccess
+    } catch (error) {   
+        loggerError(error)
+        return false
+    }
+}
+
+const get = async (key: string) => {
+    try {
+        const value = await redis.get(key)
+
+        return value
+    } catch (error) {   
+        loggerError(error)
+    }
+}
+
+export {
+    set,
+    get,
+}
