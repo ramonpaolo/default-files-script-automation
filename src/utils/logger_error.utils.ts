8ad82b12c625b2fff665b3a95d4a3b988d351cca
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2023-01-20 17:19:37 -0300

    feat: show only ipv4 address of user

--- src/utils/logger_error.utils.ts
+++ src/utils/logger_error.utils.ts
@@ -5,10 +5,14 @@ import { Request } from 'express'
 import logger from '../settings/logger.settings'
 
 const loggerError = (error: Error, request?: Request, optional?: object) => {
-  if (request != null)
-    logger.error({
+  if (request != null) {
+    const ipV4 = request.ip.split(':').pop()
+
+    return logger.error({
       request: {
-        request_ip: request.ip,
+        request_ip: {
+          request_ip_v4: ipV4,
+        },
         request_path: request.path,
         request_body: request.body
       },
@@ -19,15 +23,16 @@ const loggerError = (error: Error, request?: Request, optional?: object) => {
         ...optional
       }
     })
-  else
-    logger.error({
-      error: {
-        error_name: error.name,
-        error_message: error.message,
-        error_stack: error.stack?.split('\n'),
-        ...optional
-      }
-    })
+  }
+
+  return logger.error({
+    error: {
+      error_name: error.name,
+      error_message: error.message,
+      error_stack: error.stack?.split('\n'),
+      ...optional
+    }
+  })
 }
 
 export default loggerError
