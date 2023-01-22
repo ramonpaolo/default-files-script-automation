29003ec340db1211da252da5e73e1a9cc4141673
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2023-01-22 10:11:59 -0300

    feat: add util logger to log info

--- src/utils/logger.utils.ts
+++ src/utils/logger.utils.ts
@@ -35,4 +35,29 @@ const loggerError = (error: Error, request?: Request, optional?: object) => {
   })
 }
 
-export default loggerError
+const loggerInfo = (message: string, optional?: object, request?: Request) => {
+  if (request === undefined) return logger.info({
+    status: 'success',
+    message,
+    ...optional
+  })
+
+  const ipV4 = request.ip.split(':').pop()
+  return logger.info({
+    status: 'success',
+    message,
+    ...optional,
+    request: {
+      request_ip: {
+        request_ip_v4: ipV4,
+      },
+      request_path: request.path,
+      request_body: request.body,
+    },
+  })
+}
+
+export {
+  loggerInfo,
+  loggerError,
+}
