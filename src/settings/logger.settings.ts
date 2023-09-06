0c95f7e3e543d282497248f4e8dedb53d79ac270
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2023-09-06 17:53:33 -0300

    feat: improve logger

--- src/settings/logger.settings.ts
+++ src/settings/logger.settings.ts
@@ -1,21 +1,27 @@
 import winston from 'winston';
 import { hostname } from 'os';
 
+const { DD_API_KEY, APP_ENV } = process.env
+
 const serviceName = ''
 
-// Enable send logs to DataDog
+
 const httpOptions: winston.transports.HttpTransportOptions = {
     host: 'http-intake.logs.datadoghq.com',
-    path: `/api/v2/logs?dd-api-key=${process.env.DATADOG_API_KEY}&ddsource=nodejs&service=${serviceName}-${process.env.NODE_ENV}&hostname=${hostname()}`,
-    ssl: true
+    path: `/api/v2/logs?dd-api-key=${DD_API_KEY}&ddsource=nodejs&service=${serviceName}-${APP_ENV}&hostname=${hostname()}`,
+    ssl: true,
 }
 
 const logger = winston.createLogger({
-    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json(), winston.format.prettyPrint()),
     transports: [
         new winston.transports.Console(),
         new winston.transports.Http(httpOptions)
-    ]
+    ],
+    defaultMeta: {
+        service: serviceName,
+        env: APP_ENV,
+    },
 })
 
 export default logger
